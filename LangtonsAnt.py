7e9032a03bc323d1e482954fac59d97a24d6fc0e
Craig Beaman <Craig.Beaman@unb.ca>
2020-07-23 13:07:35 -0300

    Now more than one ant can run at the same time.
    Also can now add random initial conditions.

--- LangtonsAnt.py
+++ LangtonsAnt.py
@@ -1,10 +1,13 @@
 from graphics import *
 import time
+import random
 
 win = GraphWin("Langton's Ant", 1000, 1000)
 game_board = []
 WIDTH = 100
 HEIGHT = 100
+# Controls what percent of squares start white/black.
+PER_WHITE_INIT = 100
 
 
 class Ant:
@@ -20,12 +23,6 @@ class Ant:
 
     def counter_clock_wise_rotation(self):
         self.orientation = (self.orientation - 1) % 4
-
-    def draw_ant_square(self):
-        sq = Rectangle(Point(self.x * 10, self.y * 10), Point((self.x + 1) * 10, (self.y + 1) * 10))
-        sq.setOutline(color_rgb(0, 0, 0))
-        sq.setFill(color_rgb(255, 0, 0))
-        sq.draw(win)
 
     def ant_move(self):
         if self.orientation == 0:
@@ -45,6 +42,7 @@ class GameSquare:
         self.square_x = square_x
         self.square_y = square_y
         self.state = 0
+        self.rectangle = Rectangle(Point(square_x * 10, square_y * 10), Point((square_x + 1) * 10, (square_y + 1) * 10))
 
     def flip_state(self):
         if self.state == 0:
@@ -55,15 +53,28 @@ class GameSquare:
     def get_state(self):
         return self.state
 
+    def set_sq_state(self, state):
+        self.state = state
+
+    def draw_square(self, is_ant=False):
+        if is_ant:
+            self.rectangle.setOutline(color_rgb(0, 0, 0))
+            self.rectangle.setFill(color_rgb(255, 0, 0))
+        elif self.state == 0:
+            self.rectangle.setOutline(color_rgb(0, 0, 0))
+            self.rectangle.setFill(color_rgb(255, 255, 255))
+        else:
+            self.rectangle.setOutline(color_rgb(255, 255, 255))
+            self.rectangle.setFill(color_rgb(0, 0, 0))
+
+    def init_drawing(self):
+        self.rectangle.draw(win)
+
 
 def main():
-    for j in range(0, WIDTH):
-        for i in range(0, HEIGHT):
-            draw_square(j, i)
-
     init_game_board()
-    ant = Ant(50, 50)
-    loop_forever(ant)
+    ant_list = [Ant(50, 50), Ant(45, 58), Ant(30, 73), Ant(30, 30)]
+    loop_forever(ant_list)
 
     win.getMouse()
     win.close()
@@ -73,44 +84,77 @@ def init_game_board():
     for i in range(0, HEIGHT):
         x = []
         for j in range(0, WIDTH):
-            x.append(GameSquare(j, i))
+            obj = GameSquare(i, j)
+            x.append(obj)
+
+            # Randomly picks starting square color based on the distribution that you want
+            if random.randint(1, 100) <= PER_WHITE_INIT:
+                obj.set_sq_state(0)
+                obj.draw_square()
+            else:
+                obj.set_sq_state(1)
+                obj.draw_square()
+
+            obj.init_drawing()
         game_board.append(x)
 
 
-def loop_forever(ant):
+def loop_forever(ant_list):
+    already_flipped_squares = []
     while True:
-        ant.draw_ant_square()
 
-        # update the square that the ant was just on
-        ant_x_pos = ant.x
-        ant_y_pos = ant.y
-        try:
-            game_board[ant_x_pos][ant_y_pos].flip_state()
-        except IndexError:
+        # Remove ants that have gone off the sides
+        for ant in ant_list.copy():
+            ant_x_pos = ant.x
+            ant_y_pos = ant.y
+            if ant_x_pos > 99 or ant_x_pos < 0 or ant_y_pos > 99 or ant_y_pos < 0:
+                ant_list.remove(ant)
+                print("An ant has died RIP")
+
+        if len(ant_list) == 0:
+            print("All ants have died :(")
             break
 
-        draw_square(ant_x_pos, ant_y_pos, game_board[ant_x_pos][ant_y_pos].get_state())
+        # Draw ant squares
+        for ant in ant_list:
+            ant_x_pos = ant.x
+            ant_y_pos = ant.y
+            sq = get_game_sq(ant_x_pos, ant_y_pos)
+            sq.draw_square(is_ant=True)
 
-        # Update the ant's position
-        if game_board[ant_x_pos][ant_y_pos].get_state() == 0:
-            ant.clock_wise_rotation()
-        else:
-            ant.counter_clock_wise_rotation()
+        # time.sleep(0.25)
 
-        ant.ant_move()
+        for ant in ant_list:
+            ant_x_pos = ant.x
+            ant_y_pos = ant.y
+
+            # Checks if more than one ant are on the same square. Only one flip allowed per cycle.
+            if not already_flipped(ant_x_pos, ant_y_pos, already_flipped_squares):
+                get_game_sq(ant_x_pos, ant_y_pos).flip_state()
+                already_flipped_squares.append([ant_x_pos, ant_y_pos])
+
+            get_game_sq(ant_x_pos, ant_y_pos).draw_square()
+
+            # Update the ant's position
+            if game_board[ant_x_pos][ant_y_pos].get_state() == 0:
+                ant.clock_wise_rotation()
+            else:
+                ant.counter_clock_wise_rotation()
+
+            ant.ant_move()
+
+        already_flipped_squares.clear()
 
 
-def draw_square(x, y, state=0):
-    sq = Rectangle(Point(x * 10, y * 10), Point((x + 1) * 10, (y + 1) * 10))
+def already_flipped(ant_x_pos, ant_y_pos, already_flipped_squares):
+    for x, y in already_flipped_squares:
+        if ant_x_pos == x and ant_y_pos == y:
+            return True
+    return False
 
-    if state == 0:
-        sq.setOutline(color_rgb(0, 0, 0))
-        sq.setFill(color_rgb(255, 255, 255))
-    else:
-        sq.setOutline(color_rgb(255, 255, 255))
-        sq.setFill(color_rgb(0, 0, 0))
 
-    sq.draw(win)
+def get_game_sq(x, y):
+    return game_board[x][y]
 
 
 main()
